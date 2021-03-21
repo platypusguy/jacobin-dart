f1adcab9e3f3e2aa54567438ba6b7af726a90b32
platypusguy <920630+platypusguy@users.noreply.github.com>
2021-03-20 22:43:37 -0700

    Added processing of command-line params passed through environment variables

--- bin/argsProcessor.dart
+++ bin/argsProcessor.dart
@@ -6,18 +6,43 @@
 
 import 'dart:io' show Platform;
 
+import 'globals.dart' as env;
+import 'notification_handler.dart';
+
 ///Processes all the args to the running program
 
 class ArgsProcessor {
   List<String> cliArgs; // strings passed on the command line when Jacobin invoked
-  String envArgs;       // strings passed to the JDK via environmental variables
+  var argTable = new Map<String, String>(); // holds the parameters and their arguments, if any
 
   ArgsProcessor( List<String> args ) {
     cliArgs = args;
   }
+  
+  ///the main function. It gathers all the CLI parameters and puts then into a table,
+  ///which holds each param and its value (if any). The talbe is then stored in the globals.
+  void process() {
+    String commandLine = gatherArgs();
+    
+    //copy the unparsed command line to globals for future reference and log it.
+    env.Globals.commandLine = commandLine;
+    env.Globals.logger.log( "Command line: ${env.Globals.commandLine}", INFO );
 
-  void gatherArgs() {
-    envArgs = getEnvArgs();
+    //parse the command line into parameters and put them into a table/map.
+    parseCommandLine( commandLine, argTable );
+  }
+
+  //gathers all the switches and CLI params from all the various sources and 
+  //creates a full command line.
+  String gatherArgs() {
+    String envArgs = getEnvArgs(); //first get the args stored in environmental variables
+    var sb = new StringBuffer( envArgs );
+    if( sb.isNotEmpty ) sb.write( " " );
+
+    //now add the args that were on the command-line when the program was run
+    sb.writeAll( cliArgs, " " );
+    String completeCommandLine = sb.toString().trimRight();
+    return completeCommandLine;
   }
 
   ///get args from the many places that the JVM supports beyond just the command line. Then,
@@ -37,8 +62,18 @@ class ArgsProcessor {
 
     return( evolvingCommandLine.toString().trimRight() );
   }
+  
+  //takes a string that is part or all of the command line and 
+  // parses it into parameters/switches and their values and places them into a table/map
+  parseCommandLine( String commandLine, Map<String,String> argsTable ) {
+      List<String> args = commandLine.split( " " );
+      for( String arg in args ) {
+        //CURR resume here. e.g., check for -XX: and see if they have any associated numeric values
+      }
+  }
 
-  ///several environment variables contains arg to be added to the command-line. this fetches them
+  ///several environment variables contains arg to be added to the command-line.
+  ///this function fetches them
   String getArgsFromEnvironment( String envVariable ) {
     String javaToolOptions = Platform.environment[envVariable];
     return( javaToolOptions == null ? "" : javaToolOptions );
